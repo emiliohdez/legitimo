ba1d1955a75873b496cbf65068944ffc81f95575
Emilio Hdez <emiliohdezhfno@gmail.com>
2019-11-06 16:47:17 -0600

    master

--- evento_nov/assets/js/main.js
+++ evento_nov/assets/js/main.js
@@ -59,9 +59,9 @@ $(document).ready(function() {
     });
 
     $(".countdown")
-        .countdown("2018/03/01", function(event) {
+        .countdown("2020/04/20", function(event) {
             $(this).html(
-                event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
+                event.strftime('<div>%D <span>Days</span></div><div>%H<span>Hours</span></div><div>%M<span>Minutes</span></div><div>%S<span>Seconds</span></div>')
             );
         });
 
